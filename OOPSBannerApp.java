ecf054a83d8962830016b1d6f5f1736cb0ef6daf
Collins-Ce <collins.cecil09@gmail.com>
2026-03-12 03:19:19 +0530

    Final Changes

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -14,13 +14,13 @@ public class OOPSBannerApp {
 
         String[] lines = new String[7];
 
-        lines[0] = String.join(" ", "  ***  ", "  ***  ", "*****  ", " ***** ");
-        lines[1] = String.join(" ", "**   **", "**   **", "**   **", "**     ");
-        lines[2] = String.join(" ", "**   **", "**   **", "**   **", "**     ");
-        lines[3] = String.join(" ", "**   **", "**   **", "*****  ", " ***** ");
-        lines[4] = String.join(" ", "**   **", "**   **", "**     ", "    ** ");
-        lines[5] = String.join(" ", "**   **", "**   **", "**     ", "    ** ");
-        lines[6] = String.join(" ", "  ***  ", "  ***  ", "**     ", " ***** ");
+        lines[0] = String.join(" ", "  *****  ", "  *****  ", "*********", " ********");
+        lines[1] = String.join(" ", " **   ** ", " **   ** ", " **    **", "**       ");
+        lines[2] = String.join(" ", "**     **", "**     **", "**    ** ", "**       ");
+        lines[3] = String.join(" ", "**     **", "**     **", "*********", " ********");
+        lines[4] = String.join(" ", "**     **", "**     **", "**       ", "       **");
+        lines[5] = String.join(" ", " **   ** ", " **   ** ", "**       ", "       **");
+        lines[6] = String.join(" ", "  *****  ", "  *****  ", "**       ", " ********");
 
         for (String line : lines) {
             System.out.println(line);
